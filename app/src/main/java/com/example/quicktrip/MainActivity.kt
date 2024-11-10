f355662d32180f2f5e1ff4e340a1f797d8eb1d92
daplopez <daphnelopez@utexas.edu>
2024-11-10 17:02:40 -0600

    added space between components

--- app/src/main/java/com/example/quicktrip/MainActivity.kt
+++ app/src/main/java/com/example/quicktrip/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.quicktrip.ui.theme.QuickTripTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,13 +36,17 @@ class MainActivity : ComponentActivity() {
             var tipPercent by remember { mutableStateOf(15f) }
 
             QuickTripTheme {
-                Column(modifier = Modifier.fillMaxSize()) {
+                Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
                     BillAmountInput(billAmount = billAmount, onValueChange = { billAmount = it })
+
+                    Spacer(modifier = Modifier.height(16.dp))
 
                     TipPercentageSlider(
                         tipPercent = tipPercent,
                         onValueChange = { tipPercent = it }
                     )
+
+                    Spacer(modifier = Modifier.height(16.dp))
 
                     TipAndTotalDisplay(tipPercent = tipPercent, totalAmount = billAmount)
                 }
