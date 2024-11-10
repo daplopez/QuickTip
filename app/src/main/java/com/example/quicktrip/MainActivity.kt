98953031b9b3681de27f173035caf983b352d12f
daplopez <daphnelopez@utexas.edu>
2024-11-10 02:15:02 -0600

    created bill amount input composable

--- app/src/main/java/com/example/quicktrip/MainActivity.kt
+++ app/src/main/java/com/example/quicktrip/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.quicktrip.ui.theme.QuickTripTheme
 
@@ -18,12 +27,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var billAmount by remember { mutableStateOf("") }
+
             QuickTripTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(modifier = Modifier.fillMaxSize()) {
+                    BillAmountInput(billAmount = billAmount, onValueChange = { billAmount = it })
                 }
             }
         }
@@ -31,17 +39,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun BillAmountInput(billAmount: String, onValueChange: (String) -> Unit) {
+    TextField(
+        value = billAmount,
+        onValueChange = onValueChange,
+        label = { Text("Enter bill amount:") },
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BillAmountInputPreview() {
     QuickTripTheme {
-        Greeting("Android")
+        BillAmountInput(billAmount = ""){ }
     }
 }
